ca3da415f82d9ee5c2f8b6d2cbd7b006de978459
Javier Soto <antonio.javier.soto.molina@gmail.com>
2024-10-02 21:06:42 +0100

    adding functions for drinks and excersise

--- data_base_setup.sql
+++ data_base_setup.sql
@@ -4,6 +4,16 @@ CREATE TABLE foods (
     type VARCHAR(50) NOT NULL
 );
 
+
+create table exercise_records(
+id SERIAL primary key,
+excersise_type varchar (50),
+time_min int);
+
+alter table exercise_records add column date DATE;
+
+select * from exercise_records;
+
 INSERT INTO foods (food, type) VALUES
 ('Salmon', 'Proteins'),
 ('Chicken', 'Proteins'),
@@ -77,6 +87,10 @@ INSERT INTO foods (food, type) VALUES
 ('Tahini', 'Fats'),
 ('Margarine', 'Fats');
 
+
+INSERT INTO foods (food, type) VALUES
+('Chicken ham', 'proteins');
+
 CREATE TABLE days_of_week (
     id SERIAL PRIMARY KEY,
     day VARCHAR(10) NOT NULL
@@ -104,6 +118,14 @@ CREATE TABLE records (
     date DATE NOT NULL
 );
 
+
+CREATE TABLE drink_records (
+    id SERIAL PRIMARY KEY,
+    drink_type VARCHAR(50) NOT NULL,
+    units DECIMAL(6, 2) NOT NULL,
+    date DATE NOT NULL
+);
+
 CREATE TABLE extras (
     id SERIAL PRIMARY KEY,
     type VARCHAR(50) NOT NULL,
@@ -112,11 +134,129 @@ CREATE TABLE extras (
 );
 
 
+create table public.kcal_per_100g (
+id SERIAL primary key,
+foodID integer,
+kcal_per_100g integer);
+
+-- Creating the table
+CREATE TABLE public.kcal_per_100g (
+    id SERIAL PRIMARY KEY,
+    foodID INTEGER,
+    kcal_per_100g INTEGER
+);
+
+-- Inserting data into the table
+INSERT INTO public.kcal_per_100g (foodID, kcal_per_100g)
+VALUES
+(1, 206),
+(2, 165),
+(3, 250),
+(4, 132),
+(5, 155),
+(6, 76),
+(7, 116),
+(8, 164),
+(9, 123),
+(10, 120),
+(11, 579),
+(12, 654),
+(13, 81),
+(14, 189),
+(15, 59),
+(16, 42),
+(17, 402),
+(18, 98),
+(19, 345),
+(20, 389),
+(21, 247),
+(22, 357),
+(23, 111),
+(24, 77),
+(25, 86),
+(26, 354),
+(27, 86),
+(28, 378),
+(29, 364),
+(30, 218),
+(31, 89),
+(32, 52),
+(33, 57),
+(34, 69),
+(35, 47),
+(36, 32),
+(37, 61),
+(38, 60),
+(39, 50),
+(40, 30),
+(41, 34),
+(42, 160),
+(43, 41),
+(44, 18),
+(45, 16),
+(46, 17),
+(47, 25),
+(48, 15),
+(49, 23),
+(50, 55),
+(51, 25),
+(52, 31),
+(53, 149),
+(54, 40),
+(55, 16),
+(56, 22),
+(57, 20),
+(58, 884),
+(59, 717),
+(60, 892),
+(61, 115),
+(62, 584),
+(63, 486),
+(64, 534),
+(65, 553),
+(66, 562),
+(67, 559),
+(68, 160),
+(69, 567),
+(70, 595),
+(71, 717),
+(72, 265),
+(73, 247),
+(74, 265),
+(75, 123);
+
+
+create table max_units_per_day (
+id SERIAL primary key,
+type varchar(50),
+max_unit int);
+
+insert into max_units_per_day (type, max_unit)
+VALUES
+('Proteins', 6),
+('Carbohydrates', 5),
+('Fats', 6),
+('Dairy', 3),
+('Fruits', 3),
+('Vegetables', 8);
+
+
+
+create table drinks_records (
+id SERIAL primary key,
+drink_type varchar(50),
+units int,
+date DATE);
+
+
 create table drinks (
 id SERIAL primary key,
 drink_type varchar (50) not NULL
 );
 
+alter table drinks add column kcal_per_drink int;
+
+delete from drinks;
 INSERT INTO drinks (drink_type) VALUES
 ('Tea'),
 ('Coffee'),
@@ -125,8 +265,69 @@ INSERT INTO drinks (drink_type) VALUES
 ('Spirit');
 
 
+
+
+
+
+INSERT INTO drinks (drink_type, kcal_per_drink) 
+VALUES
+('Tea', 5),            -- 2 kcal per 100ml -> 5 kcal per 250ml
+('Coffee', 3),         -- 1 kcal per 100ml -> 3 kcal per 250ml
+('Beer', 108),         -- 43 kcal per 100ml -> 108 kcal per 250ml
+('Coke', 105),         -- 42 kcal per 100ml -> 105 kcal per 250ml
+('Spirit', 625),       -- 250 kcal per 100ml -> 625 kcal per 250ml
+('Wine', 213),         -- 85 kcal per 100ml -> 213 kcal per 250ml
+('Water', 0),          -- 0 kcal per 100ml -> 0 kcal per 250ml
+('Juice', 113),        -- 45 kcal per 100ml -> 113 kcal per 250ml
+('Milkshake', 375),    -- 150 kcal per 100ml -> 375 kcal per 250ml
+('Smoothie', 325),     -- 130 kcal per 100ml -> 325 kcal per 250ml
+('Lemonade', 100),     -- 40 kcal per 100ml -> 100 kcal per 250ml
+('Iced Tea', 75),      -- 30 kcal per 100ml -> 75 kcal per 250ml
+('Hot Chocolate', 225),-- 90 kcal per 100ml -> 225 kcal per 250ml
+('Soda', 98),          -- 39 kcal per 100ml -> 98 kcal per 250ml
+('Energy Drink', 275), -- 110 kcal per 100ml -> 275 kcal per 250ml
+('Whiskey', 625),      -- 250 kcal per 100ml -> 625 kcal per 250ml
+('Rum', 575),          -- 230 kcal per 100ml -> 575 kcal per 250ml
+('Gin', 650),          -- 260 kcal per 100ml -> 650 kcal per 250ml
+('Vodka', 588),        -- 235 kcal per 100ml -> 588 kcal per 250ml
+('Champagne', 225);    -- 90 kcal per 100ml -> 225 kcal per 250ml
+
+-- Creating the exercise table
+CREATE TABLE public.exercise (
+    id SERIAL PRIMARY KEY,
+    exercise_name VARCHAR(50),
+    avg_kcal_burnt_30min INTEGER
+);
+
+-- Inserting data into the exercise table
+INSERT INTO public.exercise (exercise_name, avg_kcal_burnt_30min) 
+VALUES
+('Walking', 150),
+('Swimming', 250),
+('Running (6 min per km)', 300),
+('Running (5:30 min per km)', 330),
+('Running (5 min per km)', 360),
+('Running (4:30 min per km)', 400),
+('HIIT', 400),
+('Cycling (moderate pace)', 300),
+('Jumping rope', 400),
+('Yoga', 150),
+('Pilates', 180),
+('Strength training', 220),
+('Rowing', 300),
+('Hiking', 250),
+('Dancing', 200),
+('Boxing', 400),
+('Tennis', 300),
+('Basketball', 280),
+('Elliptical trainer', 270),
+('Stair climbing', 350);
+
+
+
 ----------------------------
 ----------------------------
 alter table public.records add column meal_type varchar(50);
-select * from public.records;
-SELECT * FROM public.foods;
+select * from public.records order by date, meal_type ;
+select * from public.foods;
+select * FROM public.drinks_records;
--- food_counter.py
+++ food_counter.py
@@ -27,10 +27,7 @@ def connect_to_db(column_name, table_name):
         cursor = conn.cursor()
         
         # Define the SQL query to select data
-        query = sql.SQL("SELECT {col} FROM public.{tbl}").format(
-            col=sql.Identifier(column_name),
-            tbl=sql.Identifier(table_name)
-        )
+        query = sql.SQL(f"SELECT {column_name} FROM public.{table_name} order by {column_name}")
         
         # Execute the query
         cursor.execute(query)
@@ -84,6 +81,73 @@ def submit_to_db(food_type, grams, date, meal_type):
         messagebox.showerror("Database Error", f"An error occurred while inserting data: {e}")
 
 
+# Function to submit data to PostgreSQL
+def submit_excersise_to_db(drink_type, units, date):
+    try:
+        # Connect to your PostgreSQL database
+        conn = psycopg2.connect(
+            dbname="dieta",
+            user="postgres",
+            password="postgres",
+            host="localhost",
+            port="5432"
+        )
+        cursor = conn.cursor()
+        
+        # Define the SQL query to insert data
+        insert_query = sql.SQL(
+            "INSERT INTO public.exercise_records (excersise_type, time_min, date) "
+            "VALUES (%s, %s, %s)"
+        )
+        data_to_insert = (drink_type, units, date)
+        
+        # Execute the query and commit the transaction
+        cursor.execute(insert_query, data_to_insert)
+        conn.commit()
+        
+        # Close the connection
+        cursor.close()
+        conn.close()
+        
+        messagebox.showinfo("Database Update", "Excercise entry successfully inserted into the database.")
+    except Exception as e:
+        messagebox.showerror("Database Error", f"An error occurred while inserting data: {e}")
+
+
+
+
+# Function to submit data to PostgreSQL
+def submit_drink_to_db(drink_type, units, date):
+    try:
+        # Connect to your PostgreSQL database
+        conn = psycopg2.connect(
+            dbname="dieta",
+            user="postgres",
+            password="postgres",
+            host="localhost",
+            port="5432"
+        )
+        cursor = conn.cursor()
+        
+        # Define the SQL query to insert data
+        insert_query = sql.SQL(
+            "INSERT INTO public.drinks_records (drink_type, units, date) "
+            "VALUES (%s, %s, %s)"
+        )
+        data_to_insert = (drink_type, units, date)
+        
+        # Execute the query and commit the transaction
+        cursor.execute(insert_query, data_to_insert)
+        conn.commit()
+        
+        # Close the connection
+        cursor.close()
+        conn.close()
+        
+        messagebox.showinfo("Database Update", "Food entry successfully inserted into the database.")
+    except Exception as e:
+        messagebox.showerror("Database Error", f"An error occurred while inserting data: {e}")
+
 # Function to process food input
 def submit_food():
     global grams, meal_type, food_type, date
@@ -107,6 +171,54 @@ def submit_food():
     
     except ValueError:
         messagebox.showerror("Input Error", "Please enter a valid number for grams.")
+
+
+# Function to process drinks input
+def submit_drink():
+    global drink_type, units, date
+    
+    try:
+        units = int(drink_units_entry.get())  
+        drink_type = drink_type_combobox.get()
+        date = calendar.get_date()
+
+        
+        if drink_type and units:
+            message = (f"{date}: You have entered {units} units of {drink_type}"
+                       f"for {drink_type}.")
+            messagebox.showinfo("Food Input Received", message)
+            
+            # Call function to submit data to the PostgreSQL database
+            submit_drink_to_db(drink_type, units, date)
+        else:
+            messagebox.showwarning("Input Error", "Please select a food category, meal category, and food type.")
+    
+    except ValueError:
+        messagebox.showerror("Input Error", "Please enter a valid number for drink units.")
+
+# Function to process drinks input
+def submit_excersise():
+    global excersise_type, time_min, date
+    
+    try:
+        time_min = int(time_units_entry.get())  
+        excersise_type =  excersise_type_combobox.get()
+        date = calendar.get_date()
+
+        
+        if time_min and excersise_type:
+            message = (f"{date}: You have entered {time_min} minutes of {excersise_type}"
+                       f"for {excersise_type}.")
+            messagebox.showinfo("Food Input Received", message)
+            
+            # Call function to submit data to the PostgreSQL database
+            submit_excersise_to_db(excersise_type, time_min, date)
+        else:
+            messagebox.showwarning("Input Error", "Please select a food category, meal category, and food type.")
+    
+    except ValueError:
+        messagebox.showerror("Input Error", "Please enter a valid number for drink units.")
+
 
 
 # Create the main window
@@ -150,6 +262,55 @@ food_type_combobox.pack()
 # Submit button for food
 submit_food_button = tk.Button(root, text="Submit Food", command=submit_food)
 submit_food_button.pack()
+
+
+#########DRINKS
+
+#### drink input section
+drink_units_label = tk.Label(root, text="Enter units of drink:")
+drink_units_label.pack()
+
+drink_units_entry = tk.Entry(root)
+drink_units_entry.pack()
+
+
+# Dropdown menu for drink type
+drink_type_label = tk.Label(root, text="Select type of drink:")
+drink_type_label.pack()
+
+drink_types = connect_to_db("drink_type","drinks")
+
+drink_type_combobox = ttk.Combobox(root, values=drink_types)
+drink_type_combobox.set("")  # Ensure no selection at startup
+drink_type_combobox.pack()
+
+# Submit button for drink
+submit_drink_button = tk.Button(root, text="Submit drink", command=submit_drink)
+submit_drink_button.pack()
+
+#########excersiseS
+
+#### excersise input section
+excersise_units_label = tk.Label(root, text="Enter min of excersise:")
+excersise_units_label.pack()
+
+time_units_entry = tk.Entry(root)
+time_units_entry.pack()
+
+
+# Dropdown menu for excersise type
+excersise_type_label = tk.Label(root, text="Select type of excersise:")
+excersise_type_label.pack()
+
+excersise_types = connect_to_db("exercise_name","exercise")
+
+excersise_type_combobox = ttk.Combobox(root, values=excersise_types)
+excersise_type_combobox.set("")  # Ensure no selection at startup
+excersise_type_combobox.pack()
+
+# Submit button for excersise
+submit_excersise_button = tk.Button(root, text="Submit excersise", command=submit_excersise)
+submit_excersise_button.pack()
 
 # Run the application
 root.mainloop()
